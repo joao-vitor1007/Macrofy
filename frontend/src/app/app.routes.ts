51fc83e2d791273feb23a301385c15e0b9515e7e
João Vitor <joaovitorbatista0104@gmail.com>
2026-05-01 17:58:37 -0300

    Configurado rota padrão

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 import { Cadastro } from './cadastro/cadastro';
 
-export const routes: Routes = [{ path: 'cadastro', component: Cadastro }];
+export const routes: Routes = [
+  // Rota padrão: quando o caminho for vazio, redireciona para 'cadastro'
+  { path: '', redirectTo: 'cadastro', pathMatch: 'full' },
+
+  { path: 'cadastro', component: Cadastro },
+];
