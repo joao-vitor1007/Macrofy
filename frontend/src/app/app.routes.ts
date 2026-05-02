a7d9ec61334857b0fa5cc06361250abf4711ca85
João Vitor <joaovitorbatista0104@gmail.com>
2026-05-02 19:20:42 -0300

    adicionado passos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Cadastro } from './cadastro/cadastro';
+import { PassosComponent } from './passos/passos';
 
 export const routes: Routes = [
   // Rota padrão: quando o caminho for vazio, redireciona para 'cadastro'
   { path: '', redirectTo: 'cadastro', pathMatch: 'full' },
 
   { path: 'cadastro', component: Cadastro },
+  { path: 'passos', component: PassosComponent },
 ];
