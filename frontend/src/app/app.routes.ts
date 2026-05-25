33e520b88d8a3835e3bf81fb2ae18ca652c6cb30
joao_vc_batista <joaovitorbatista0104@gmail.com>
2026-05-25 20:19:54 -0300

    adicionado rota de dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Cadastro } from './cadastro/cadastro';
 import { PassosComponent } from './passos/passos';
 import { Login } from './login/login';
+import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
   // Rota padrão: quando o caminho for vazio, redireciona para 'cadastro'
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: 'cadastro', component: Cadastro },
   { path: 'passos', component: PassosComponent },
   { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard}
 ];
